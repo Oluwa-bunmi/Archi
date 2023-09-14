636af4ac82425e178197312f25f8dce9080fb5b4
Jummy007 <gbemisoyeadejumoke@gmail.com>
2023-09-14 01:40:53 +0100

    booking form in progress

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -16,13 +16,15 @@ const Booking = () => {
       </div>
       <section className="bg-[#F7F5F0] py-[90px]">
         <section className="px-5 md:px-44">
-          <h1>Choose room type</h1>
+          <h1 className="text-xl font-garamond font-semibold mb-5 text-[#222]">
+            Choose room type
+          </h1>
           <div className="grid grid-cols-3 text-center gap-5">
             {suitesInfo.map((item, i) => (
               <div
                 key={i}
                 onClick={() => handleImageClick(item.room)}
-                className="transition-transform"
+                className="transition-transform text-[#999] text-[18px]"
               >
                 <img
                   src={item.image}
@@ -30,27 +32,95 @@ const Booking = () => {
                   className={`${
                     selectedRoom === item.room
                       ? "border-[12px] border-[#bf9156]"
-                      : ""
+                      : "mb-[10px]"
                   }`}
                 />
-                <h3>{item.room}</h3>
-                <h3>{item.price}</h3>
+                <h3 className="font-normal font-roboto">{item.room}</h3>
+                <h3
+                  className={`${
+                    selectedRoom === item.room
+                      ? "text-[#333] font-bold"
+                      : "font-bold"
+                  }`}
+                >
+                  {item.price}
+                </h3>
               </div>
             ))}
           </div>
-          <div className="">
-            <select
-              value={selectedRoom}
-              onChange={(e) => setSelectedRoom(e.target.value)}
-            >
-              <option value="">Select a Room</option>
-              {suitesInfo.map((item, i) => (
-                <option key={i} value={item.room}>
-                  {item.room}
-                </option>
-              ))}
-            </select>
-          </div>
+          <form action="" className="mt-8">
+            <section className="grid grid-cols-4 gap-5 text-base font-normal">
+              <div>
+                <label
+                  htmlFor=""
+                  className="text-[#222] text-xl font-garamond font-semibold"
+                >
+                  Check in
+                </label>
+                <br />
+                <input
+                  type="date"
+                  name=""
+                  id=""
+                  className="w-full p-[10px] mt-5 bg-transparent outline-none rounded-[5px] border border-borderHr"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor=""
+                  className="text-[#222] text-xl font-garamond font-semibold"
+                >
+                  Check Out
+                </label>
+                <br />
+                <input
+                  type="date"
+                  name=""
+                  id=""
+                  className="w-full p-[10px] text-[#333] mt-5 bg-transparent outline-none rounded-[5px] border border-borderHr"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor=""
+                  className="text-[#222] text-xl font-garamond font-semibold"
+                >
+                  Guests
+                </label>
+                <br />
+                <select className="w-full p-[10px] text-[#333] mt-5 bg-transparent outline-none rounded-[5px] border border-borderHr">
+                  <option value="">1</option>
+                  <option value="">1-3</option>
+                  <option value="">1-6</option>
+                </select>
+              </div>
+
+              <div className="">
+                <label
+                  htmlFor=""
+                  className="text-[#222] text-xl font-garamond font-semibold"
+                >
+                  Room
+                </label>
+                <br />
+                <select
+                  value={selectedRoom}
+                  onChange={(e) => setSelectedRoom(e.target.value)}
+                  className="w-full p-[10px] text-[#333] mt-5 bg-transparent outline-none rounded-[5px] border border-borderHr"
+                >
+                  <option value="">Select a Room</option>
+                  {suitesInfo.map((item, i) => (
+                    <option key={i} value={item.room}>
+                      {item.room}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </section>
+            <section>
+                
+            </section>
+          </form>
         </section>
       </section>
     </>
